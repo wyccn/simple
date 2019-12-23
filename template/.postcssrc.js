9baf93cd56c359e4596c68c7e5bc3756f9cffc62
wangyunchuan <wangyunchuan3410@163.com>
2019-12-23 16:23:15 +0800

    commit

--- template/.postcssrc.js
+++ template/.postcssrc.js
@@ -1,24 +1,16 @@
 /*
- * @Description: 
+ * @Description: postcss配置
  * @Author: WangYunChuan
  * @Email: wangyunchuan3410@163.com
  * @Company: BJ_TCSD
  * @Version: 1.0.0
  * @Date: 2019-06-17 16:37:57
  * @LastEditors  : WangYunChuan
- * @LastEditTime : 2019-12-23 14:33:51
+ * @LastEditTime : 2019-12-23 16:23:04
  */
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  /*"plugins": {
-    //to edit target browsers: use "browserslist" field in package.json
-    "postcss-import": {},
-    "autoprefixer": {},
-    "postcss-px2rem": {
-      "remUnit": 37.5
-    }
-  }*/
   'plugins': {
 		"postcss-import": {},
     "autoprefixer": {},
