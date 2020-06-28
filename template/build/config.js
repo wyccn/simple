610407c1be3fdf67cf008890e8b1f69d57bf3363
wangyunchuan <wangyunchuan3410@163.com>
2020-06-28 15:04:03 +0800

    删除echo插件，增加公共资源地址配置选项

--- template/build/config.js
+++ template/build/config.js
@@ -27,7 +27,7 @@ module.exports = {
         index: path.resolve(__dirname, '../dist/index.html'),
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/',
+        assetsPublicPath: "{{publicPath}}",
 				devtool: false,
 				{{#if gzip}}
 				productionGzip: true,
